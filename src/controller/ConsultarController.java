12658a7994213caacfa4c3bb29602d3add93210e
PCunh <160776541+PCunh@users.noreply.github.com>
2024-11-15 21:19:53 -0300

    Atualizei ConsultarController.java

--- src/controller/ConsultarController.java
+++ src/controller/ConsultarController.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controller;
 
-/**
- *
- * @author pedro
- */
+import modell.Usuario;
+import view.Menu;
+import view.Saldo;
+
 public class ConsultarController {
+    private Usuario usuario;
+    private Saldo view;
+
+    public ConsultarController() {
+    }
     
+    public ConsultarController(Usuario usuario, Saldo view) {
+        this.usuario = usuario;
+        this.view = view;
+    }
+    
+    public void Saldo(){
+        view.Saldo(usuario.getCarteira());
+    }
+    
+    public void Menu(){
+        Menu menu = new Menu(usuario);
+        menu.setVisible(true);
+        view.dispose();
+    }
 }

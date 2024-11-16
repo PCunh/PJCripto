b9b413eab933f9fa919d43041088bbee383a6a34
PCunh <160776541+PCunh@users.noreply.github.com>
2024-11-15 21:14:32 -0300

    Atualizei Conexao.java

--- src/DAO/Conexao.java
+++ src/DAO/Conexao.java
@@ -1,13 +1,17 @@
 
 package DAO;
 
+//Todos os import's nescessários e que linka o banco de dados
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
+/*Aqui começa uma classe pública que
+linka com o banco de dados atráves do link abaixo*/
 public class Conexao {
     public Connection getConnection() throws SQLException{
-        //Nesta linha debaixo falta colocar o link do banco de dados
-        Connection conexao = DriverManager.getConnection();
+        Connection conexao = DriverManager.getConnection
+        ("jdbc:postgresql://localhost:5432/Usuários","postgres","PEDRO");
         return conexao;
+    }
 }

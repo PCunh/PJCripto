d27f7c8adf1b9184afa67b1db4d73e6e06cc0fd8
PCunh <160776541+PCunh@users.noreply.github.com>
2024-11-15 21:19:02 -0300

    Atualizei ComprarController.java

--- src/controller/ComprarController.java
+++ src/controller/ComprarController.java
@@ -1,6 +1,35 @@
 
 package controller;
 
+import DAO.CarteiraDAO;
+import javax.swing.JOptionPane;
+import modell.Usuario;
+import view.Menu;
+
 public class ComprarController {
+    private Usuario usuario;
+    private ComprarCripto view;
+
+    public ComprarController() {
+    }
+
+    public ComprarController(Usuario usuario, ComprarCripto view) {
+        this.usuario = usuario;
+        this.view = view;
+    }
     
+    public void Comprar(Moeda moeda,double valor){
+        if(CarteiraDAO.comprarMoeda(usuario.getCarteira(), moeda, valor)){
+            JOptionPane.showMessageDialog(view, "Compra realizada com sucesso");
+            Menu();
+        }else{
+            JOptionPane.showMessageDialog(view, "ERRO");
+        }
+    }
+    
+    public void Menu(){
+        Menu menu = new Menu(usuario);
+        menu.setVisible(true);
+        view.dispose();
+    }
 }
